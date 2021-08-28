4e6d4ceca3f0db64f3ec3f63cde6b2fcb567c8f5
Aiswarya Saji <aiswarya2saji@gmail.com>
2021-08-28 15:58:10 +0530

    Deleted

--- event/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from django.forms import fields
-from .models import Event
-
-class EventForm(forms.ModelForm):
-    class Meta:
-        model = Event
-        fields = ['name','event_date','venue','description','image']
